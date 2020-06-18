01aab4ff0a5d83ea14cda04c1fd6aca734d79085
Houjf <18829536923@163.com>
2020-06-18 15:56:26 +0800

    02.VertexBuffer 1.Create a Window

--- OpenGL/02-VertexBufferAndDrawAnTriangle/src/drawAnTriangle.cpp
+++ OpenGL/02-VertexBufferAndDrawAnTriangle/src/drawAnTriangle.cpp
@@ -31,7 +31,17 @@ int main(void)
 	std::cout << glGetString(GL_VERSION) << std::endl;
 	std::cout << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 	std::cout << glGetStringi(GL_EXTENSIONS, 1) << std::endl;
-	
+
+	float positions[6] =
+	{
+		-0.5f, -0.5f,
+		 0.0f,  0.5f,
+		 0.5f, -0.5f
+	};	 
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -39,11 +49,7 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f( 0.0f,  0.5f);
-		glVertex2f( 0.5f, -0.5f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3)
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
